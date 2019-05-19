452b9dcd01396bf5eb6196e3e6a5cd599ae1d05a
Sarah Rodrigues <sarahrodriguesmoreira@gmail.com>
2019-05-19 17:39:30 -0300

    Finalizando

--- server/app.js
+++ server/app.js
@@ -1,33 +1,70 @@
-var express = require('express'),
-    app = express();
 
-// carregar "banco de dados" (data/jogadores.json e data/jogosPorJogador.json)
-// você pode colocar o conteúdo dos arquivos json no objeto "db" logo abaixo
-// dica: 3-4 linhas de código (você deve usar o módulo de filesystem (fs))
-var db = {
-};
+var express = require("express");
+var app = express();
 
+app.use(express.static("client/"));
 
-// configurar qual templating engine usar. Sugestão: hbs (handlebars)
-//app.set('view engine', '???');
+var server = app.listen(3000, function () {
+   var host = server.address().address;
+   var port = server.address().port;   
+   console.log("Listening at http://%s:%s", host, port);
+});
 
-
-// EXERCÍCIO 2
-// definir rota para página inicial --> renderizar a view index, usando os
-// dados do banco de dados "data/jogadores.json" com a lista de jogadores
-// dica: o handler desta função é bem simples - basta passar para o template
-//       os dados do arquivo data/jogadores.json
-
-// EXERCÍCIO 3
-// definir rota para página de detalhes de um jogador --> renderizar a view
-// jogador, usando os dados do banco de dados "data/jogadores.json" e
-// "data/jogosPorJogador.json", assim como alguns campos calculados
-// dica: o handler desta função pode chegar a ter umas 15 linhas de código
+app.set("view engine", "hbs");
 
 
-// EXERCÍCIO 1
-// configurar para servir os arquivos estáticos da pasta "client"
-// dica: 1 linha de código
+const fs = require("fs");
 
-// abrir servidor na porta 3000
-// dica: 1-3 linhas de código
+let rawdata = fs.readFileSync("server/data/jogadores.json");  
+let data = JSON.parse(rawdata);  
+
+let rawGameData = fs.readFileSync("server/data/jogosPorJogador.json");  
+let gameData = JSON.parse(rawGameData);
+
+app.set("views", "server/views");
+
+app.get("/", function (req, res) {
+  res.render("index.hbs", data); 
+});
+
+
+app.get("/jogador/:path/", function (req, res) {	
+	for(let player of data.players){
+		if(player.steamid == req.params.path){
+			let playerGameData = gameData[player.steamid];
+
+			let playtimeList = [];
+			let playedGames = 0;
+
+			for(let game of playerGameData.games){
+				if(game.playtime_forever != 0){
+					playedGames++;
+					playtimeList.push(game.playtime_forever);
+				}				
+			}
+
+			let ordPlaytimeList = playtimeList.sort(function(a, b){return b-a}).slice(0,5);
+			let ordPlayerGameData = [];
+			
+			for(let gameTime of ordPlaytimeList){
+				for(let game of playerGameData.games){				
+					if(game.playtime_forever == gameTime){					
+						ordPlayerGameData.push(game);	
+						break;
+ 					}
+  				}
+  			}
+
+			let data = {
+				player: player,
+				quantidadeJogos: playerGameData.game_count,
+				naoJogados: playerGameData.game_count - playedGames,
+				topFive: ordPlayerGameData,
+				top: ordPlayerGameData[0]
+			}
+			
+			res.render("jogador.hbs", data); 
+			return;
+		}
+	}
+});
